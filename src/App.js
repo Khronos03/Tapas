dc64081e00456b859a24946e7666c9de541375f8
José Suaza <Jsuaza@intergrupo.com>
2022-11-17 22:40:57 -0500

    a

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Prueba en AWS
         </a>
       </header>
     </div>
